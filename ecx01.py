7897b894c23ce74ef2e2a699a32a534f6257acb5
JuanVaz14 <167100614+JuanVaz14@users.noreply.github.com>
2025-08-27 09:33:46 -0300

    Delete ecx01.py

--- ecx01.py
+++
@@ -1,7 +0,0 @@
-#Tabuada simples
-#Crie um programa que solicita ao usuário um número inteiro. Em seguida, use um loop for para imprimir a tabuada desse número de 1 a 10.
-
-numero = int(input("Digite um número: "))
-
-for i in range(1, 11):
-    print(f"{numero} x {i} = {numero * i}")
