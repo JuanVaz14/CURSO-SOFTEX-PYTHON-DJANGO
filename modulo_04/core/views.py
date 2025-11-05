aadd60086b2242d3d1015f150212b5a6892caa77
Juan Vaz <juanoablo2005@gmail.com>
2025-11-05 18:25:22 -0300

    update

--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    context = {
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+    }
+    # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
+    return render(request, 'home.html', context)
 
 def login(request):
     return HttpResponse("<input>Login</input>")
